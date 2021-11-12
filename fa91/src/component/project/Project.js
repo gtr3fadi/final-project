e667a9a51993fc3e29ee9b03d2bc18b93330ef61
gtr3fadi <86829039+gtr3fadi@users.noreply.github.com>
2021-11-12 02:18:14 +0200

    fffdd
    design

--- fa91/src/component/project/Project.js
+++ fa91/src/component/project/Project.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import { projectFirestore } from "../../firebase/firebase";
 import { useCollection } from "../hook/useCollection";
 import { useAuthContext } from "../hook/useAuthContext";
-import Avatar from "../Avatar"
+import Avatar from "../Avatar";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 export default function Project() {
   const { user } = useAuthContext();
@@ -17,7 +18,7 @@ export default function Project() {
   };
 
   return (
-    <div className="container">
+    <div className="container my-4">
       <div className="row">
         <div className="col-md-12">
           <h2 className="text-center">Projects</h2>
@@ -26,27 +27,51 @@ export default function Project() {
               {data && <h3> Projects Total : ({data.length})</h3>}
               {data &&
                 data.map((project) => (
-                  <div className="card my-3 bg-light" key={project.id}>
+                  <div className="card my-3 bg-light " key={project.id}>
                     <div className="card-body ">
                       <h5 className="card-title text-primary text-center text-capitalize">
                         {project.projectName}
                       </h5>
                       <div className="row d-flex justify-content-between align-items-center">
-                        <p className="card-text col-4 mb-0 d-flex justify-content-start align-items-center">
+                        <p className="card-text col-4 m-0 d-flex justify-content-start align-items-center">
                           <Avatar src={project.createdBy.photoURL} />
-                          <span className="text-capitalize ms-2 font-weight-bold">
+                          <span className="text-capitalize ms-1 font-weight-bold">
                             {project.createdBy.displayName}
                           </span>
                         </p>
-                        <p className="card-text col-7 d-flex justify-content-end align-items-center">
-                          <span className="font-weight-bold">Due by : </span>
-                          {project.projectDuration.toDate().toDateString()}
-                        </p>
+                        {project.projectDuration.toDate() < new Date() ? (
+                          <p className="card-text col-8 m-0 d-flex justify-content-end align-items-center">
+                            <span className="text-capitalize m-0 font-weight-bold text-danger small">
+                              Expired Date :
+                              {formatDistanceToNow(
+                                project.projectDuration.toDate()
+                              )}
+                            </span>
+                          </p>
+                        ) : (
+                          <p className="card-text col-8 mb-0 d-flex justify-content-end align-items-center">
+                            <span className="text-capitalize ms-2 font-weight-bold small ">
+                              Bidding Ends In :
+                              {formatDistanceToNow(
+                                project.projectDuration.toDate()
+                              )}
+                            </span>
+                          </p>
+                        )}
                       </div>
-                      <hr/>
-                      <p className="card-text">
-                        <span className="font-weight-bold">Description : </span>
-                        {project.projectDescription}</p>
+                      <hr />
+                      {project.projectDuration.toDate() < new Date() ? (
+                        <p class="note note-danger">
+                          <strong>Description :</strong>
+                          {project.projectDescription}
+                        </p>
+                      ) : (
+                        <p class="note note-primary">
+                          <strong>Description :</strong>
+                          {project.projectDescription}
+                        </p>
+                      )}
+                     
                       <p className="card-text">
                         <span className="font-weight-bold">
                           {" "}
@@ -70,7 +95,12 @@ export default function Project() {
                         View
                       </Link>
                       <p className="card-text text-right">
-                        {project.createdAt.toDate().toLocaleString()}
+                        <small className="text-muted">
+                          Posted :{" "}
+                          {formatDistanceToNow(project.createdAt.toDate(), {
+                            addSuffix: true,
+                          })}
+                        </small>
                       </p>
                     </div>
                   </div>
@@ -84,4 +114,3 @@ export default function Project() {
     </div>
   );
 }
-
